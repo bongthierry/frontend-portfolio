bd25c3f9fb135648a15ad2c7624a10c436b3ae3e
thierry19-ops <121738479+thierry19-ops@users.noreply.github.com>
2023-05-17 22:25:00 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/portfolio;    
+import App from './App';
+
+reactDOM.render(<APP/> , document.getElementById('root'));//This line indicates that all the content of this file should be rendered in the root id of the html file//
